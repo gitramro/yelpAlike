9b88478cc40d25daf537c82755d14e7718c3e12e
gitramro <ramon.osc93@gmail.com>
2017-05-18 23:41:48 +0000

    add mongo lab

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes   = require("./routes/comments"),
     indexRoutes     = require("./routes/index")
      
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://ramro:password@ds147711.mlab.com:47711/_yelpcamp");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
