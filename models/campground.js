3f2b57428544b5651083746d6c33dc78e5d97dcd
gitramro <ramon.osc93@gmail.com>
2017-05-18 07:36:39 +0000

    First commit

---
+++ models/campground.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+
+//SCHEMA SETUP CAMPGROUNDS
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    price: String,
+    image: String,
+    description:  String,
+    location: String,
+    lat: Number,
+    lng: Number,
+    createdAt: {type: Date, default: Date.now},
+    author:{
+      id:{
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User"
+      } ,
+      username: String
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId, //we are embeding a reference to the comment (id)  not embeding the actual comment
+            ref: "Comment" //name of model
+        }
+    ]
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
---
+++ routes/index.js
@@ -0,0 +1,62 @@
+var express=require("express");
+var router=express.Router();
+var passport=require("passport");
+var User=require("../models/user");
+
+//Root Route
+router.get("/",function(req,res){
+    res.render("landing");
+});
+
+//Show Register Form
+router.get("/register", function(req,res){
+    res.render("register");
+});
+
+//Handle Signup Logic
+router.post("/register", function(req,res){
+   var newUser= new User({username:req.body.username});     //provided by passport local mongoose package
+   User.register(newUser, req.body.password, function(err,user){
+       if(err){
+        req.flash("error", err.message);
+        return res.redirect("/register");
+       }
+       passport.authenticate("local")(req,res, function(){
+           req.flash("success", "Welcome to YelpCamp "+user.username+" !");
+           return res.redirect("/campgrounds");
+       });
+   });
+});
+
+//Show Login Form
+router.get("/login",function(req,res){
+    res.render("login");
+});
+
+//Handling Login Logic
+router.post("/login",passport.authenticate("local",
+    {
+     successRedirect:"/campgrounds",
+     failureRedirect:"/login" 
+    }),function(req,res){
+});
+
+//Logout Route
+router.get("/logout",function(req,res){
+    req.logout();
+    req.flash("success","Logged you out!");
+    res.redirect("/campgrounds");
+});
+
+// show register form
+router.get("/register", function(req, res){
+   res.render("register", {page: 'register'}); 
+});
+
+//show login form
+router.get("/login", function(req, res){
+   res.render("login", {page: 'login'}); 
+});
+
+
+module.exports=router;
